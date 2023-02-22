a56f6bb8e5c9d8e0e4a3f93ddcf6b60ed23df427
“xxgzzp” <xxgzzp@gmail.com>
2023-02-22 19:07:32 +0800

    基本框架

---
+++ src/pages/Contract/ContractList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ContractList.less';
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page ContractList</h1>
+    </div>
+  );
+}
